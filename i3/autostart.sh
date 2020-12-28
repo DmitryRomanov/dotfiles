b111eb0ca8168ba54feeeb85e6084392a4a3f765
Dmitry Romanov <dmitry.romanov85@gmail.com>
2020-12-28 12:23:52 +0700

    start polybar

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,10 +1,11 @@
 xrandr --output HDMI-A-0 --left-of DisplayPort-0 --rotate left --auto
 nitrogen --restore
 redshift-gtk &
-gxkb &
 dropbox start &
 nm-applet &
-telegram-desktop -startintray &
+telegram-desktop &
+polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini second &
 xcompmgr -c -f -nv &
 xautolock -time 15 -locker '~/.local/bin/fuzzy_lock.sh' &
 /opt/OpenDrive/odrive &
