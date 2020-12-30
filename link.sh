0b51a6c84e3a50794a7d440a714a805ce8267463
Dmitry Romanov <dmitry.romanov85@gmail.com>
2020-12-30 12:04:30 +0700

    add fontconfig

--- link.sh
+++ link.sh
@@ -3,3 +3,5 @@ ln -nsf `pwd`/rofi/ ~/.config/
 ln -nsf `pwd`/termite/ ~/.config/
 ln -nsf `pwd`/.i3status.conf ~/
 ln -nsf `pwd`/polybar/ ~/.config/
+ln -nsf `pwd`/fontconfig/ ~/.config/
+fc-cache -v
