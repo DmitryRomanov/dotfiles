ce6423b416000119f1027c142b96c806760af758
Dmitry Romanov <dmitry.romanov85@gmail.com>
2021-09-15 15:31:36 +0700

    sway configs

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,12 +1,10 @@
 xrandr --output HDMI-A-0 --left-of DisplayPort-0 --rotate left --auto
 nitrogen --restore
 redshift-gtk &
-dropbox start &
 nm-applet &
-QT_QPA_PLATFORMTHEME=qt5ct TDESKTOP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY=1 telegram-desktop &
+QT_QPA_PLATFORMTHEME=qt5ct TDESKTOP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY=1 kotatogram-desktop &
 polybar -c ~/.config/polybar/config.ini main &
 polybar -c ~/.config/polybar/config.ini second &
 xcompmgr -c -f -nv &
 xautolock -time 15 -locker '~/.local/bin/fuzzy_lock.sh' &
-/opt/OpenDrive/odrive &
 setxkbmap "us,ru" ",winkeys" "grp:win_space_toggle" &
--- link.sh
+++ link.sh
@@ -1,8 +1,10 @@
 ln -nsf `pwd`/i3/ ~/.config/
+ln -nsf `pwd`/i3 ~/.config/sway
 ln -nsf `pwd`/rofi/ ~/.config/
 ln -nsf `pwd`/termite/ ~/.config/
 ln -nsf `pwd`/.i3status.conf ~/
 ln -nsf `pwd`/polybar/ ~/.config/
+ln -nsf `pwd`/waybar/ ~/.config/
 ln -nsf `pwd`/fontconfig/ ~/.config/
 ln -nsf `pwd`/kitty/ ~/.config/
 fc-cache -v
