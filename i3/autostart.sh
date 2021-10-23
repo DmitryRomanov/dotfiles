eaea87ac2e5a661b3eeb3a0daa07b9b50c047767
Dmitry Romanov <dmitry.romanov85@gmail.com>
2021-10-23 21:10:58 +0700

    use i3 status

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,10 +1,12 @@
 xrandr --output HDMI-A-0 --left-of DisplayPort-0 --rotate left --auto
 nitrogen --restore
 redshift-gtk &
+gxkb &
 nm-applet &
 QT_QPA_PLATFORMTHEME=qt5ct TDESKTOP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY=1 kotatogram-desktop &
-polybar -c ~/.config/polybar/config.ini main &
-polybar -c ~/.config/polybar/config.ini second &
+#polybar -c ~/.config/polybar/config.ini main &
+#polybar -c ~/.config/polybar/config.ini second &
 xcompmgr -c -f -nv &
 xautolock -time 15 -locker '~/.local/bin/fuzzy_lock.sh' &
 setxkbmap "us,ru" ",winkeys" "grp:win_space_toggle" &
+feh --randomize --bg-fill ~/Yandex.Disk/обои\ вертикальные --bg-fill ~/Yandex.Disk/обои
